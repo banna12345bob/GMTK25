be0f0ffe9c23e1a977616b3ac72055650a703ac0
banna12345bob <banna12345bob@outlook.com>
2025-07-25 15:47:52 +1000

    Triangle!!!

--- engine/src/engine/ImGui/ImGuiRenderer.cpp
+++ engine/src/engine/ImGui/ImGuiRenderer.cpp
@@ -17,14 +17,14 @@ namespace Engine {
 		ImGui::CreateContext();
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking		
 		// Just be mindful that for some reason external ImGUI viewports are laggy when VSync is enabled
 		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 		ImGui::StyleColorsDark();
 
-
 		SDL_Window* window = static_cast<SDL_Window*>(Application::getApplication()->getWindow()->getNativeWindow());
+
 		ImGui_ImplSDL3_InitForOpenGL(window, SDL_GL_GetCurrentContext());
 		ImGui_ImplOpenGL3_Init("#version 330");
 	}
@@ -43,14 +43,16 @@ namespace Engine {
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplSDL3_NewFrame();
 		ImGui::NewFrame();
-		for (int i = 0; i < this->getImGuiLayersSize(); i++)
-			this->getImGuiLayer(i)->renderImGUILayer();
+
+		for (int i = 0; i < getImGuiLayersSize(); i++)
+			getImGuiLayer(i)->renderImGUILayer();
+
+		ImGuiIO& io = ImGui::GetIO();
+		io.DisplaySize = ImVec2(Application::getApplication()->getWindow()->GetWidth(), Application::getApplication()->getWindow()->GetHeight());
 
 		ImGui::Render();
-
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
--- engine/src/platform/SDL/SDLWindow.cpp
+++ engine/src/platform/SDL/SDLWindow.cpp
@@ -225,6 +225,83 @@ namespace Engine {
 			// Clears the viewport
 			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			float vertices[] = {
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.0f,  0.5f, 0.0f
+			};
+
+			unsigned int VBO;
+			glGenBuffers(1, &VBO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+			// ===================== Creates the vertex shader ===========================
+			const char* vertexShaderSource = "#version 330 core\n"
+				"layout (location = 0) in vec3 aPos;\n"
+				"void main()\n"
+				"{\n"
+				"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+				"}\0";
+
+			unsigned int vertexShader;
+			vertexShader = glCreateShader(GL_VERTEX_SHADER);
+			glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+			glCompileShader(vertexShader);
+
+
+			// ================== Creates the fragment shader ============================
+			const char* fragmentShaderSource = "#version 330 core\n"
+				"out vec4 FragColor;\n"
+				"void main()\n"
+				"{\n"
+				"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+				"}\0";
+
+			unsigned int fragmentShader;
+			fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+			glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+			glCompileShader(fragmentShader);
+
+			// ================== Creates the combined shader ============================
+			unsigned int shaderProgram;
+			shaderProgram = glCreateProgram();
+			glAttachShader(shaderProgram, vertexShader);
+			glAttachShader(shaderProgram, fragmentShader);
+			glLinkProgram(shaderProgram);
+			glUseProgram(shaderProgram);
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
+
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+
+			// Vertex buffer object
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+			glUseProgram(shaderProgram);
+
+			// Vertex array object
+			unsigned int VAO;
+			glGenVertexArrays(1, &VAO);
+
+			glBindVertexArray(VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+			glUseProgram(shaderProgram);
+			glBindVertexArray(VAO);
+
+
+			// Draw verticies using a VAO and our shader
+			glUseProgram(shaderProgram);
+			glBindVertexArray(VAO);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 		}
 		Application::getApplication()->getImGuiRenderer()->Render();
 
